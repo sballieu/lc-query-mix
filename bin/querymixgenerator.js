198e8081ca07f0190f9886a5f380514cab823bdc
Sander <sander.ballieu@ugent.be>
2016-04-07 23:06:11 +0200

    Make commandline tool for generator +  add filter on user agent

--- bin/querymixgenerator.js
+++ bin/querymixgenerator.js
@@ -1,12 +1,39 @@
 #!/usr/bin/node
 var csv = require('fast-csv'),
     fs = require('fs'),
-    moment = require('moment');
+    moment = require('moment'),
+    program = require('commander');
 
-var stream = fs.createReadStream("2015-10-routeplanning-logs-sorted.csv");
+console.error("Queyr mix generator use --help to discover more functions");
+
+var config;
+
+program
+    .version('0.1.0')
+    .option('-f --filter [string]', 'filter on user agent')
+    .arguments('<configFile>')
+    .action(function (query) {
+        try {
+            query = fs.readFileSync(query, { encoding: 'utf8' });
+        } catch (error) {
+            console.error(error);
+        }
+        config = JSON.parse(query);
+    })
+    .parse(process.argv);
+
+if (!config) {
+    console.error('Please provide a config file.');
+    process.exit();
+}
+
+var user_agent_filter = program.filter|| "";
+user_agent_filter = user_agent_filter.toLowerCase();
+
+var stream = fs.createReadStream(config.queries_file);
 
 //start time
-var t0 = moment("2015-10-01T16:00:00+01:00");
+var t0 = moment(config.start_time);
 
 var csvStream = csv({headers:true})
     .on("data", function(data) {
@@ -14,7 +41,7 @@ var csvStream = csv({headers:true})
       if (data.requested_datetime) {
         data.requested_datetime = moment(data.requested_datetime);
       }
-      if (data.datetime >= t0) {
+      if (data.datetime >= t0 && data.user_agent.toLowerCase().indexOf(user_agent_filter) > -1) {
         var object = {};
         var delta = (data.datetime - t0)/1000; //in seconds
         object.T =  delta % 3600; //When to execute a request

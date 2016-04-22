3b26fd53893732c41effc8411525d8609a3ce6ad
Pieter Colpaert <pieter.colpaert@okfn.org>
2016-04-22 17:07:43 +0200

    This repo focuses on creating a query mix only, not executing the benchmark

--- bin/querymixexecutor.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-
-var ExecuteQueryMixTransform = require('../lib/ExecuteQueryMixTransform');
-var JSONStream = require('jsonstream');
-process.stdin.pipe(JSONStream.parse()).pipe(new ExecuteQueryMixTransform({
-  type : 'query-serve'
-}));

6ef77afebe1bff44ac5b362d473fb11e2a2d771f
Pieter Colpaert <pieter.colpaert@okfn.org>
2016-04-28 14:15:05 +0200

    Correct Time difference now

--- bin/generator.js
+++ bin/generator.js
@@ -8,6 +8,8 @@ var stream = fs.createReadStream("2015-10-routeplanning-logs-sorted.csv");
 //start time
 var t0 = moment("2015-10-01T16:00:00+01:00");
 
+var interval = 15*60; // 15 minutes
+
 var csvStream = csv({headers:true})
     .on("data", function(data) {
       data.datetime = moment(data.datetime);
@@ -18,10 +20,11 @@ var csvStream = csv({headers:true})
         var object = {};
         var delta = (data.datetime - t0)/1000; //in seconds
         object.T =  delta % 3600; //When to execute a request
+        object.round = Math.round(delta / interval); //the amount of intervals we are running in front
         object.departureStop = data.from_id;
-        object.arrivalStop = data.to_id;
+        object.arrivalStop = data.to_id;        
         //Round the departure time by minute
-        object.departureTime = new Date(Math.round(((data.requested_datetime || data.datetime) - delta * 1000)/1000)*1000);
+        object.departureTime = new Date(Math.round(((data.requested_datetime || data.datetime) - object.round * interval * 1000)/1000)*1000);
         object.originalDepartureTime = data.requested_datetime || data.datetime;
         console.log(JSON.stringify(object));
       }
